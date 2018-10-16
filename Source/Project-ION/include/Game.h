03828b4c69cfe7c2f7b541970b7f260630d0ae26
Otávio Bertucini <otaviobertucini@gmail.com>
2018-10-16 08:45:23 -0300

    Class game added and UML updated

---
+++ Source/Project-ION/include/Game.h
@@ -0,0 +1,20 @@
+//#ifndef GAME_H
+//#define GAME_H
+//#include <allegro.h>
+//#include <stdlib.h>
+//#include <time.h>
+//#include <cstddef> //library for NULL
+//#include <iostream>
+//
+//class Game
+//{
+//public:
+//    Game();
+//    ~Game();
+//    void
+//private:
+//
+//
+//};
+//
+//#endif // GAME_H
---
+++ Source/Project-ION/src/Game.cpp
@@ -0,0 +1,13 @@
+//#include "Game.h"
+//
+//Game::Game()
+//{
+//    allegro_init();
+//    install_keyboard();
+//    set_color_depth(32);
+//}
+//
+//Game::~Game()
+//{
+//    //dtor
+//}
