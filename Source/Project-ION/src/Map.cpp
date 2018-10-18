64c5f1665f112e55243de379dd63da0a0ab4a64e
Otávio Bertucini <otaviobertucini@gmail.com>
2018-10-18 15:18:15 -0300

    Fixed bug in collision with edge #2

--- Source/Project-ION/src/Map.cpp
+++ Source/Project-ION/src/Map.cpp
@@ -14,7 +14,7 @@ int** Map::getMatrix(){
 /*Returns the value of the matrix in the given (i, j) position
 If at least one of the values is invalid, NULL is returned.*/
 int Map::getValueMap(int i, int j){
-    if(j > 36 || j < 0 || i > 23 || i < 0)
+    if(j > 35 || j < 0 || i > 23 || i < 0)
         return -1;
     return matrix[i][j];
 }
